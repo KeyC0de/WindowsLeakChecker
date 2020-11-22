459b4b21fd0e0463540d459a0f4a4b885b2c57f5
KeyC0de <nik.lazkey@gmail.com>
2020-11-22 21:28:06 +0200

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <iostream>
+
+#define pass (void)0
+
+void exitHandler0() 
+{
+	pass;
+}
+
+int main()
+{
+	const auto exitHandle = std::atexit( exitHandler0 );
+	if ( exitHandle != 0 )
+	{
+		std::wcerr << L"Registration failed\n";
+		return exitHandle;
+	}
+
+	return 0;
+}
